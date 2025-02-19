7036136c944147327935d3a8230bb468f4f4c981
Roihanafh <roihanalfahmi123@gmail.com>
2025-02-19 15:43:48 +0700

    Praktikum 3_Pertemuan 2

--- routes/web.php
+++ routes/web.php
@@ -71,3 +71,13 @@ Route::resource('photos', PhotoController::class)->only([
 Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
    ]);  
+Route::get('/greeting', function () {
+    return view('hello', ['name' => 'Roihan']);
+    });
+// Route::get('/greeting', function () {
+//     return view('blog.hello', ['name' => 'Roihan']);
+//     });
+Route::get('/greeting', [WelcomeController::class,
+    'greeting']);
+    
+    
